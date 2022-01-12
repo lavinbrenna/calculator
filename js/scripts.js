deaa0c3c852c1e0d64f3c05057a799a86318fdd1
Brenna Lavin <lavinbrenna@gmail.com>
2022-01-12 13:26:05 -0800

    add form functionality
    Co-authored-by: Jack Jones <jacktjones4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,34 @@ function multiply(number1, number2){
 function divide(number1, number2){
   return number1 / number2;
 }
-const number1 = parseInt(prompt("Enter a number: "));
-const number2 = parseInt(prompt("Enter another number:"));
 
-const result = fToC(number1);
-alert(result);
-
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
